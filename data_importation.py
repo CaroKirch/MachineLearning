472a1b4ff233d31044956536dbb29bb43665f2ec
Caroline KIRCH <caroline.kirch@dauphine.eu>
2024-03-14 14:30:29 +0100

    Notebook

--- data_importation.py
+++ data_importation.py
@@ -5,9 +5,8 @@ import matplotlib.pyplot as plt
 
 ###################################### Importation des données : ######################################
 
-# Il y a tellement de données que l'importation sur git se fait pas
-# J'ai du les sortir du dossier partagé, donc il faut préciser le début du chemin d'accès : 
 data_root_path = "C:/Users/kroki/Dropbox/Caroline/Documents/Cours/1 - Master/4ème semestre/Machine learning/Data/"
+# data_root_path = "/Users/a_clouet/Documents/Master_2022-2024/M2/S2/MachineLearning/Projet/"
 
 # Test Data : 
 #test_away_player_statistics = pd.read_csv(data_root_path + "Test_Data/test_away_player_statistics_df.csv", sep=",", index_col=0)
@@ -53,9 +52,15 @@ print(Y_train.describe())
 ######################################### Données manquantes : ########################################
 
 # Graphique données manquantes (cours) : 
-plt.figure(figsize=(16, 4))
-(Y_train.shape[1] - Y_train.count()).plot.bar()
-plt.show()
+def graph_missing_data(df:pd.DataFrame) :
+    plt.figure(figsize=(16, 4))
+    (df.count() - df.shape[1]).plot.bar()
+    plt.show()
 
-Y_train = Y_train.replace({np.inf:np.nan,-np.inf:np.nan})
+graph_missing_data(test_home_team_statistics)
+
+
+
+
+# Y_train = Y_train.replace({np.inf:np.nan,-np.inf:np.nan})
 
